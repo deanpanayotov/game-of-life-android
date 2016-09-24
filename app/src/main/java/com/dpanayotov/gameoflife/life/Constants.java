aa6ba0c9f44954832c391a50747e286c08cacd32
Dean Panayotov <deanpanayotov@gmail.com>
2016-09-23 20:10:21 -0700

    Tweaking.

--- app/src/main/java/com/dpanayotov/gameoflife/life/Constants.java
+++ app/src/main/java/com/dpanayotov/gameoflife/life/Constants.java
@@ -6,14 +6,12 @@ package com.dpanayotov.gameoflife.life;
 
 public class Constants {
 
-    public static final int GRID_HEIGHT = 48;
-    public static final int GRID_WIDTH = 27;
+    public static final int GRID_HEIGHT = 96;
+    public static final int GRID_WIDTH = 54;
     public static final int TICK_INTERVAL = 1000;
 
-    public static final int CELL_PADDING = 4;
-
+    public static final int CELL_PADDING = 2;
     public static final int RADIUS_STEP = 2;
-
     public static final int QUEUE_SIZE = 4;
 
 }
