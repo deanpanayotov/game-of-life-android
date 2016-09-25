39437bf25ab9cbdabf347448a109fda0c5b9d6c6
Dean Panayotov <deanpanayotov@gmail.com>
2016-09-24 23:32:07 -0700

    Added Butterknife. ValueSetSeekBar working properly (non-generic tho).

--- app/src/main/java/com/dpanayotov/gameoflife/preferences/PreferenceActivity.java
+++ app/src/main/java/com/dpanayotov/gameoflife/preferences/PreferenceActivity.java
@@ -2,8 +2,15 @@ package com.dpanayotov.gameoflife.preferences;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.dpanayotov.gameoflife.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Dean Panayotov on 9/24/2016
@@ -11,9 +18,30 @@ import com.dpanayotov.gameoflife.R;
 
 public class PreferenceActivity extends Activity {
 
+    @BindView(R.id.test)
+    ValueSetSeekBar seekBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences2);
+        ButterKnife.bind(this);
+
+        final List<Integer> vals = new ArrayList<>();
+        vals.add(1);
+        vals.add(2);
+        vals.add(3);
+        vals.add(5);
+        vals.add(8);
+        vals.add(13);
+        vals.add(21);
+
+        seekBar.setValues(vals);
+        seekBar.setOnValueChangeListener(new ValueSetSeekBar.OnValueChangeListener() {
+            @Override
+            public void onValueChange(int value) {
+                Toast.makeText(PreferenceActivity.this, "" + value, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/dpanayotov/gameoflife/preferences/ValueSetSeekBar.java
+++ app/src/main/java/com/dpanayotov/gameoflife/preferences/ValueSetSeekBar.java
@@ -2,11 +2,18 @@ package com.dpanayotov.gameoflife.preferences;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.dpanayotov.gameoflife.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Dean Panayotov on 9/24/2016
@@ -14,10 +21,18 @@ import com.dpanayotov.gameoflife.R;
 
 public class ValueSetSeekBar extends LinearLayout {
 
+    @BindView(R.id.title)
     TextView title;
+    @BindView(R.id.value)
     TextView value;
+    @BindView(R.id.suffix)
     TextView suffix;
+    @BindView(R.id.seekbar)
     SeekBar seekBar;
+
+    List<Integer> set;
+
+    OnValueChangeListener onValueChangeListener;
 
     public ValueSetSeekBar(Context context) {
         super(context);
@@ -26,6 +41,7 @@ public class ValueSetSeekBar extends LinearLayout {
 
     public ValueSetSeekBar(Context context, AttributeSet attrs) {
         super(context, attrs);
+
         init();
     }
 
@@ -35,11 +51,47 @@ public class ValueSetSeekBar extends LinearLayout {
     }
 
     private void init() {
-        inflate(getContext(), R.layout.value_set_seekbar, this);
-        this.title = (TextView)findViewById(R.id.title);
-        this.value = (TextView)findViewById(R.id.value);
-        this.suffix = (TextView)findViewById(R.id.suffix);
-        this.seekBar = (SeekBar) findViewById(R.id.seekbar);
+        View root = inflate(getContext(), R.layout.value_set_seekbar, this);
+        ButterKnife.bind(root, this);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (set != null) {
+                    Integer val = set.get(progress);
+                    value.setText(val.toString());
+                    if(onValueChangeListener!=null){
+                        onValueChangeListener.onValueChange(val);
+                    }
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+
+    public void setValues(List<Integer> values) {
+        set = new ArrayList<>();
+        set.addAll(values);
+
+        int seekBarMax = values.size() - 1;
+        int seekBarDefault = seekBarMax / 2;
+
+        seekBar.setMax(seekBarMax);
+        seekBar.setProgress(seekBarDefault);
+    }
+
+    public void setOnValueChangeListener(OnValueChangeListener onValueChangedListener){
+        this.onValueChangeListener = onValueChangedListener;
+    }
+
+    interface OnValueChangeListener {
+        void onValueChange(int value);
     }
 
 }
