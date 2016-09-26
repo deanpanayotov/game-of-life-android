97e6f745b19d41a7373b4e05ad98a10349aa1b87
Dean Panayotov <deanpanayotov@gmail.com>
2016-09-25 17:23:11 -0700

    Resolution is a POJO again (...).

--- app/src/main/java/com/dpanayotov/gameoflife/util/Resolution.java
+++ app/src/main/java/com/dpanayotov/gameoflife/util/Resolution.java
@@ -1,13 +1,10 @@
 package com.dpanayotov.gameoflife.util;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 /**
  * Created by Dean Panayotov on 9/25/2016
  */
 
-public class Resolution implements Parcelable {
+public class Resolution {
     public int gridWidth;
     public int gridHeight;
     public int cellSize;
@@ -22,35 +19,4 @@ public class Resolution implements Parcelable {
     public String toString() {
         return gridWidth + "x" + gridHeight;
     }
-
-    protected Resolution(Parcel in) {
-        gridWidth = in.readInt();
-        gridHeight = in.readInt();
-        cellSize = in.readInt();
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(gridWidth);
-        dest.writeInt(gridHeight);
-        dest.writeInt(cellSize);
-    }
-
-    @SuppressWarnings("unused")
-    public static final Parcelable.Creator<Resolution> CREATOR = new Parcelable.Creator<Resolution>() {
-        @Override
-        public Resolution createFromParcel(Parcel in) {
-            return new Resolution(in);
-        }
-
-        @Override
-        public Resolution[] newArray(int size) {
-            return new Resolution[size];
-        }
-    };
 }
